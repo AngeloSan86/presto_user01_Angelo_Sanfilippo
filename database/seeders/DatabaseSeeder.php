d676ee0ab6de9ff35e3918b148ec0cac5bdea1e2
Angelo Sanfilippo <angelosanfilippoct@gmail.com>
2024-10-21 11:52:53 +0200

    User Story 1 completata

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Database\Seeders\DatabaseSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public $categories = [
+        'elettronica',
+        'abbigliamento',
+        'salute e bellezza',
+        'casa e giardinaggio',
+        'giocattoli',
+        'sport',
+        'animali domestici',
+        'libri e riviste',
+        'accessori',
+        'motori'
+    ];
+
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        foreach ($this->categories as $category) {
+           Category::create([
+            'name' => $category
+           ]);
+        }
+    }
+}
