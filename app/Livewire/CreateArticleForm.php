8b66646f55b4273157f1ddb78a03f4414ee5a195
Angelo Sanfilippo <angelosanfilippoct@gmail.com>
2024-10-22 13:20:37 +0200

    User Story 5 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,12 +4,13 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
 
 class CreateArticleForm extends Component
 {
-
+    use WithFileUploads;
     #[Validate('required|min:5')]
     public $title;
     #[Validate('required|min:10')]
@@ -19,6 +20,8 @@ class CreateArticleForm extends Component
     #[Validate('required')]
     public $category;
     public $article;
+    public $images = [];
+    public $temporary_images;
 
     public function store()
     {
@@ -31,9 +34,14 @@ class CreateArticleForm extends Component
             'user_id' => Auth::id()
         ]);
 
-        $this->reset();
+        if (count($this->images) > 0) {
+            foreach ($this->images as $image) {
+                $this->article->images()->create(['path' => $image->store('images', 'public')]);
+            }
+        }
 
         session()->flash('success', 'Articolo creato correttamente');
+        $this->cleanForm();
     }
 
 
@@ -41,4 +49,32 @@ class CreateArticleForm extends Component
     {
         return view('livewire.create-article-form');
     }
+
+    public function updatedTemporaryImages()
+    {
+        if ($this->validate([
+            'temporary_images.*' => 'image|max:1024',
+            'temporary_images' => 'max:6'
+        ])) {
+            foreach ($this->temporary_images as $image) {
+                $this->images[] = $image;
+            }
+        }
+    }
+
+    public function removeImage($key)
+    {
+        if (in_array($key, array_keys($this->images))) {
+            unset($this->images[$key]);
+        }
+    }
+
+    protected function cleanForm()
+{
+    $this->title = '';
+    $this->description = '';
+    $this->category = '';
+    $this->price = '';
+    $this->images = [];
+}
 }
--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -5,37 +5,44 @@
         </div>
     </div>
 
-    <div class="row height-custom justify-content-center py-5">
+      <div class="row height-custom justify-content-center py-5">
         <div class="col-12 col-md-6 mb-3">
+    @if ($article->images->count())
         <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="http://picsum.photos/399" class="d-block w-100" alt="Immagine prodotto">
-    </div>
-    <div class="carousel-item">
-      <img src="http://picsum.photos/400" class="d-block w-100" alt="Immagine prodotto">
-    </div>
-    <div class="carousel-item">
-      <img src="http://picsum.photos/401" class="d-block w-100" alt="Immagine prodotto">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-        </div>
-        <div class="col-12 col-md-6 mb-3 height-custom text-center">
-            <h2 class="display-5"> <span class="fw-bold">Titolo: span</span> {{ $article->title }}</h2>
-            <div class="d-flex flex-column justify-content-center h-75">
-                <h4 class="fw-bold">Prezzo: {{ $article->price }} €</h4>
-                <h5>Descrizione:</h5>
-                <p>{{ $article->description }}</p>
+            <div class="carousel-inner">
+                @foreach ($article->images as $key => $image)
+                    <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                        <img src="{{ Storage::url($image->path) }}" class="d-block w-100 rounded shadow"
+                             alt="Immagine {{ $key + 1 }} dell'articolo {{ $article->title }}">
+                    </div>
+                @endforeach
             </div>
+            @if ($article->images->count() > 1)
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            @endif
         </div>
+    @else
+        <img src="https://picsum.photos/300" alt="Nessuna foto inserita dall'utente">
+    @endif
+</div>
+      </div>
+
+      <div class="col-12 col-md-6 mb-3 height-custom text-center">
+          <h2 class="display-5"> <span class="fw-bold">Titolo: span</span> {{ $article->title }}</h2>
+          <div class="d-flex flex-column justify-content-center h-75">
+              <h4 class="fw-bold">Prezzo: {{ $article->price }} €</h4>
+              <h5>Descrizione:</h5>
+              <p>{{ $article->description }}</p>
+          </div>
+      </div>
     </div>
 </x-layout>
